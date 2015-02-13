a687e858bb1ad36306be40a8b9259f36d3d91f73
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-13 10:06:09 -0500

    use `.match()`

--- test.js
+++ test.js
@@ -16,37 +16,37 @@ var helper = require('./');
 describe('sync', function () {
   it('should generate API docs from the given file:', function () {
     var res = helper.sync("fixtures/a.js");
-    (/### \[\.one\]/.test(res)).should.be.true;
+    res.should.match(/### \[\.one\]/);
   });
 
   it('should generate API docs from a glob of files:', function () {
     var res = helper.sync("fixtures/*.js");
-    (/### \[\.one\]/.test(res)).should.be.true;
-    (/### \[\.four\]/.test(res)).should.be.true;
+    res.should.match(/### \[\.one\]/);
+    res.should.match(/### \[\.four\]/);
   });
 
   it('should work as a lodash helper:', function () {
-    var res = _.template('<%= apidocs("fixtures/a.js") %>', {}, {imports: {apidocs: helper.sync}});
-    (/### \[\.one\]/.test(res)).should.be.true;
+    var res = _.template('<%= apidocs("fixtures/a.js") %>', {imports: {apidocs: helper.sync}})({});
+    res.should.match(/### \[\.one\]/);
   });
 
   it('should work as a lodash mixin:', function () {
     _.mixin({apidocs: helper.sync});
-    var res = _.template('<%= _.apidocs("fixtures/a.js") %>', {});
-    (/### \[\.one\]/.test(res)).should.be.true;
+    var res = _.template('<%= _.apidocs("fixtures/a.js") %>')({});
+    res.should.match(/### \[\.one\]/);
   });
 
   it('should work as a handlebars helper:', function () {
     handlebars.registerHelper('apidocs', helper.sync);
     var res = handlebars.compile('{{apidocs "fixtures/a.js"}}')();
-    (/### \[\.one\]/.test(res)).should.be.true;
+    res.should.match(/### \[\.one\]/);
   });
 
   it('should work with Template:', function () {
     var template = new Template();
     template.helper('apidocs', helper.sync);
     template.page('docs', {content: '<%= apidocs("fixtures/a.js") %>'});
-    (/### \[\.one\]/.test(template.render('docs'))).should.be.true;
+    template.render('docs').should.match(/### \[\.one\]/);
   });
 });
 
@@ -57,21 +57,21 @@ describe('helper apidocs', function () {
     template.page('docs', {content: '<%= apidocs("fixtures/a.js") %>'});
     template.render('docs', function (err, content) {
       if (err) return done(err);
-      (/### \[\.one\]/.test(content)).should.be.true;
+      content.should.match(/### \[\.one\]/);
       done();
     });
   });
 
   it('should generate API docs from the given file:', function () {
     helper("fixtures/a.js", function (err, content) {
-      (/### \[\.one\]/.test(content)).should.be.true;
+      content.should.match(/### \[\.one\]/);
     });
   });
 
   it('should generate API docs from a glob of files:', function () {
     helper("fixtures/*.js", function (err, content) {
-      (/### \[\.one\]/.test(content)).should.be.true;
-      (/### \[\.four\]/.test(content)).should.be.true;
+      content.should.match(/### \[\.one\]/);
+      content.should.match(/### \[\.four\]/);
     });
   });
 });
