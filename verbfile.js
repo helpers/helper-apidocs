d1fb2b69e0db6bc5c217fdc26334b0465786e9ee
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-11 09:52:52 -0500

    update verbfile

--- verbfile.js
+++ verbfile.js
@@ -1,10 +1,19 @@
-var verb = require('verb');
+'use strict';
 
-// this helper is already included in verb,
-// it's only used here for tests
-verb.asyncHelper('apidocs', require('./'));
+module.exports = function(verb) {
+  verb.data({nickname: 'apidocs'});
 
-verb.task('default', function () {
-  verb.src('.verb.md')
-    .pipe(verb.dest('.'));
-});
+  // this helper is already included in verb,
+  // it's only used here for tests
+  verb.asyncHelper('apidocs', require('./'));
+  verb.helper('wrap', function(name) {
+    return '{%= ' + name + '(\'index.js\') %}';
+  });
+
+  verb.task('default', function () {
+    console.log(verb)
+    return verb.src('.verb.md')
+      .pipe(verb.renderFile('text'))
+      .pipe(verb.dest('.'))
+  });
+};
